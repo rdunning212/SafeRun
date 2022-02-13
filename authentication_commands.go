9399d05d75cda731d836c7e14c4ea29e7ac49234
Ryan Dunning <ryan.dunning@bluecore.com>
2022-02-13 15:36:03 -0500

    Add refresh command

--- authentication_commands.go
+++ authentication_commands.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	uuid "github.com/satori/go.uuid"
 	"golang.org/x/crypto/bcrypt"
 	"net/http"
@@ -91,4 +92,51 @@ func SignIn(w http.ResponseWriter, r *http.Request) {
 	})
 	// If we reach this point, that means the users password was correct, and that they are authorized
 	// The default 200 status is sent
+}
+
+func Refresh(w http.ResponseWriter, r *http.Request) {
+	// (BEGIN) The code uptil this point is the same as the first part of the `Welcome` route
+	c, err := r.Cookie("session_token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	sessionToken := c.Value
+
+	response, err := cache.Do("GET", sessionToken)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if response == nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	// (END) The code uptil this point is the same as the first part of the `Welcome` route
+
+	// Now, create a new session token for the current user
+	newSessionToken := uuid.NewV4().String()
+	_, err = cache.Do("SETEX", newSessionToken, "120", fmt.Sprintf("%s", response))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Delete the older session token
+	_, err = cache.Do("DEL", sessionToken)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Set the new token as the users `session_token` cookie
+	http.SetCookie(w, &http.Cookie{
+		Name:    "session_token",
+		Value:   newSessionToken,
+		Expires: time.Now().Add(120 * time.Second),
+	})
 }
